3103238d0bbd8a1833541505af1eb694d6e1360e
Rushinalawade <nalawaderushikesh11@gmail.com>
2022-06-01 08:53:58 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Myprojects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
